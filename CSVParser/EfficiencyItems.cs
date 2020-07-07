2aaa36f390bb643fc1143c2d941f35761e65dbd5
MaxNickol <internprogr97@gmail.com>
2020-07-07 17:40:48 +0300

    The latest version

---
+++ CSVParser/EfficiencyItems.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CSVParser
+{
+   public class EfficiencyItems
+    {
+
+        public int Rank { get; set; }
+
+        public string Title { get; set; }
+
+        public double Efficiency { get; set; }
+
+        public int Cost { get; set; }
+
+        
+
+
+        public override string ToString()
+        {
+            return $"Rank = {Rank}, Title is {Title}, Efficiency = {Efficiency}, Cost = {Cost}";
+        }
+
+        public static EfficiencyItems ParserCsv (string lines)
+        {
+            string[] parts = lines.Split(';');
+
+            return new EfficiencyItems
+            {
+                Rank = int.Parse(parts[0]),
+                Title = parts[1],
+                Efficiency =double.Parse(parts[2]),
+                Cost = int.Parse(parts[3]),
+                
+            };
+        }
+
+    }
+}
---
+++ CSVParser/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CSVParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadingFile(@"TopItemsGold.csv");
+        }
+
+
+
+        public static void ReadingFile(string path)
+        {
+
+
+            List<EfficiencyItems> list = File.ReadAllLines(path)
+                                              .Skip(1)
+                                              .Select(line => EfficiencyItems.ParserCsv(line))                                              
+                                              .OrderByDescending(item => item.Cost)
+                                              .Take(10)
+                                              .ToList();
+
+            Console.WriteLine($"The most expensive item in list is {list.Max(item => item.Title + " Efficiency " + item.Efficiency + " Cost " + item.Cost)}");
+            Console.WriteLine();
+            Console.WriteLine($"The cheapest item in list is {list.Min(item => item.Title + " Efficiency " + item.Efficiency + " Cost " + item.Cost)} ");
+            Console.WriteLine();            
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+                                              
+            
+        }
+    }
+}
